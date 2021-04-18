5932708be8907059ad7c68547b9bbd7ed4346890
Vikrant <vikranty2j@gmail.com>
2021-04-18 22:52:42 +0530

    transform state to game history

--- src/App.js
+++ src/App.js
@@ -1,40 +1,53 @@
-import React,{useState} from "react";
-import Board  from './components/Board';
-import { calculateWinner } from "./helper";
+import React, { useState } from 'react';
+import Board from './components/Board';
+import { calculateWinner } from './helper';
+
 import './styles/root.scss';
-const App= () => {
 
-  
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [isXNext,setIsNext] =useState(false);
-  
-  const winner = calculateWinner(board);
-  const message= winner ? `Winner is ${winner}`: `Next player is ${isXNext? 'X' : 'O'}`;
+const App = () => {
+  const [history, setHistory] = useState([
+    { board: Array(9).fill(null), isXNext: true },
+  ]);
+  const [currentMove, setCurrentMove] = useState(0);
+
+  const current = history[currentMove];
+
+  console.log('history', history);
+
+  const winner = calculateWinner(current.board);
+  const message = winner
+    ? `Winner is ${winner}`
+    : `Next player is ${current.isXNext ? 'X' : 'O'}`;
+
   const handelSquareClick = position => {
- 
-    if(board[position]||winner)
-    return;
-    setBoard(prev => {
+    if (current.board[position] || winner) {
+      return;
+    }
 
-      return prev.map((square,pos) => {
+    setHistory(prev => {
+      const last = prev[prev.length - 1];
 
+      const newBoard = last.board.map((square, pos) => {
         if (pos === position) {
-
-          return isXNext ? 'X' :'O';
-
+          return last.isXNext ? 'X' : 'O';
         }
-        return square;
 
+        return square;
       });
 
+      return prev.concat({ board: newBoard, isXNext: !last.isXNext });
     });
-    setIsNext( (prev)  => !(prev));
+
+    setCurrentMove(prev => prev + 1);
   };
 
-  return(<div className='app'>
-    <h1>TIC TAC TOE</h1>
-    <h2>{message}</h2>
-    <Board board={board} handelSquareClick={handelSquareClick}/>
-  </div>);
+  return (
+    <div className="app">
+      <h1>TIC TAC TOE</h1>
+      <h2>{message}</h2>
+      <Board board={current.board} handelSquareClick={handelSquareClick} />
+    </div>
+  );
 };
+
 export default App;
